158a21c2bf8976143ea8a5f7d4d796d7e475a254
snovozhilov <sergey.novozhilov@upm.ai>
2021-09-05 16:36:49 +0300

    move Procfile

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager.main'
+    'main'
 ]
 
 MIDDLEWARE = [

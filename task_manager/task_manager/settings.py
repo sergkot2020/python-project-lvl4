d2e268972c701f9154b077d750e9f5fca81c9269
snovozhilov <sergey.novozhilov@upm.ai>
2021-09-05 15:54:22 +0300

    add django-heroku dependency

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+
+import django_heroku
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -127,3 +129,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())

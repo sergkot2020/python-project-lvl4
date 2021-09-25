a374e85743a8d8f1d64f70bdd1327783aa431a48
snovozhilov <sergey.novozhilov@upm.ai>
2021-09-25 17:58:50 +0300

    added context to template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.utils.translation import gettext
+from main import const
 
 
 def main_view(request):
-    return render(request, 'index.html')
+    context = {
+        'enter': gettext('Вход'),
+        'registration': gettext('Регистрация'),
+        'users': gettext('Пользователи'),
+        'header': gettext('Привет от Хекслета!'),
+        'body': gettext('Практические курсы по программированию'),
+        'link': 'hexlet.io',
+        'footer': 'Hexlet 2021',
+    }
+    return render(request, 'index.html', context)
